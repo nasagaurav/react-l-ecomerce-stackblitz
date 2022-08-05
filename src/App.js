f072b3962c2b204c078ab935684ef580b2f19ade
nasagaurav <nasa.gauravnasa@gmail.com>
2022-08-05 15:06:53 +0530

    routing done in app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './style.css';
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="" element={<Home />} />
+          <Route path="Login" element={<Login />} />
+          <Route path="Signup" element={<Signup />} />
+          <Route path="Cart" element={<Cart />} />
+          <Route path="Orders" element={<Orders />} />
+          <Route path="Profile" element={<Profile />} />
+          <Route path="Logout" element={<Logout />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
