d31e0802caa46e389fffa67bde56ac6e8695f3cf
nasagaurav <nasa.gauravnasa@gmail.com>
2022-08-05 15:20:23 +0530

    state, dispatch added i n home

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,10 @@
-function Header() {
+import React from 'react'
+import { useSelector, useDispatch} from 'react-redux'
+
+function Header(props) {
+  const state= useState(s=>s)
+  const dispatch = useDispatch()
+  
   return <div> Header </div>;
 }
 export default Header;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useSelector, useDispatch} from 'react-redux'
 
 function Home() {
+  const state= useState(s=>s)
+  const dispatch = useDispatch()
   return <div>Home</div>;
 }
 export default Home;
