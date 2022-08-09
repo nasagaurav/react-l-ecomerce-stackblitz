9977cb0ae833ba62a98ea9f4d25901d5b962e116
nasagaurav <nasa.gauravnasa@gmail.com>
2022-08-09 07:22:46 +0530

    added status based data in services

--- src/services/index.js
+++ src/services/index.js
@@ -27,7 +27,7 @@ export async function getCart(token) {
   const result = await axios
     .get(url, headers)
     .then((res) => res.data)
-    .then((d) => d.data);
+    .then((d) => (d.status ? d.data : []));
   return result;
 }
 export async function getOrders(token) {
@@ -40,6 +40,6 @@ export async function getOrders(token) {
   const result = await axios
     .get(url, headers)
     .then((res) => res.data)
-    .then((d) => d.data);
+    .then((d) => (d.status ? d.data : []));
   return result;
 }
