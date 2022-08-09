8ded06442fc103fea0d1758234c4fb5d08ba5f29
nasagaurav <nasa.gauravnasa@gmail.com>
2022-08-09 07:45:29 +0530

    basic login form created

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 function Login(props) {
   const state = useSelector((s) => s);
   const dispatch = useDispatch();
 
-  return <div>Login</div>;
+  const r1 = useRef();
+  const r2 = useRef();
+
+  const hs = () => {
+    const email = r1.current.value;
+    const password = r2.current.value;
+
+    console.log(email, password);
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <input placeholder="email" ref={r1} />
+      <input placeholder="password" ref={r2} />
+      <button onClick={hs}>login</button>
+    </div>
+  );
 }
 export default Login;
