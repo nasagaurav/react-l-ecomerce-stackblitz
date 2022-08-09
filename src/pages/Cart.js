926b62807f40af48646a36164752f1b0dbeed792
nasagaurav <nasa.gauravnasa@gmail.com>
2022-08-09 08:13:05 +0530

    basic cart table added

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -3,7 +3,35 @@ import { useSelector, useDispatch } from 'react-redux';
 function Cart(props) {
   const state = useSelector((s) => s);
   const dispatch = useDispatch();
-
-  return <div>Cart</div>;
+  const { cart } = state;
+  return (
+    <div>
+      <h1>My Cart ({cart.length}) </h1>
+      <table>
+        <thead>
+          <tr>
+            <th>title</th>
+            <th>image</th>
+            <th>price</th>
+            <th>qty</th>
+            <th>action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {cart.map((x) => (
+            <tr>
+              <td>{x.title}</td>
+              <td>
+                <img src={x.image} width={100} />
+              </td>
+              <td>{x.new}</td>
+              <td>{x.qty}</td>
+              <td>remove</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 export default Cart;
