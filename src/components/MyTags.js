69a72741461371c09fb010ace7472258f97745f5
nasagaurav <nasa.gauravnasa@gmail.com>
2022-08-09 07:29:16 +0530

    added my tags code

--- src/components/MyTags.js
+++ src/components/MyTags.js
@@ -1,6 +1,21 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
 function MyTags(props) {
-  return <div>MyTags</div>;
+  const state = useSelector((s) => s);
+  const dispatch = useDispatch();
+
+  const { tags } = state;
+
+  return (
+    <div>
+      <div>MyTags</div>
+      {tags.map((x) => (
+        <button>
+          {x.name} {x.count}
+        </button>
+      ))}
+    </div>
+  );
 }
 export default MyTags;
