dd97707ddc2cfb8a31f3bc547f69ba95baed99be
nasagaurav <nasa.gauravnasa@gmail.com>
2022-08-09 07:36:56 +0530

    added prodicust and card

--- src/components/MyProducts.js
+++ src/components/MyProducts.js
@@ -1,6 +1,19 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import ProductCard from './ProductCard';
 
 function MyProducts(props) {
-  return <div>MyProducts</div>;
+  const state = useSelector((s) => s);
+  const dispatch = useDispatch();
+
+  const { products } = state;
+  return (
+    <div>
+      <div>MyProducts</div>
+      {products.map((x) => (
+        <ProductCard key={x._id} {...x} />
+      ))}
+    </div>
+  );
 }
 export default MyProducts;
---
+++ src/components/ProductCard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function ProductCard(props) {
+  // props._id,
+  // props._title,
+  // props.description,
+  // props.old,
+  // props.new,
+  // props.rating,
+  // props.discount,
+  // props.tags,
+  // props.image,
+
+  return <div>{JSON.stringify(props)}</div>;
+}
+
+export default ProductCard;
