ff457304f8e9e187907787eb04aa1ecea63ee861
RS <radhe212221@gmail.com>
2022-08-10 07:35:13 +0530

    added header name nasa in avatar

--- src/components/Header.js
+++ src/components/Header.js
@@ -146,7 +146,7 @@ const Header = () => {
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                <Avatar alt="Nasa" src="/static/images/avatar/2.jpg" />
               </IconButton>
             </Tooltip>
             <Menu
