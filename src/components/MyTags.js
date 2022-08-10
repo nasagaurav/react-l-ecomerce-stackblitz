48d1af2d35525655be71a9692ad63c4907e56ec3
RS <radhe212221@gmail.com>
2022-08-10 07:41:17 +0530

    My tags added with button and buttongroup with container

--- src/components/MyTags.js
+++ src/components/MyTags.js
@@ -1,5 +1,9 @@
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import * as React from "react";
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import Container from "@mui/material/Container";
+
+import { useSelector, useDispatch } from "react-redux";
 
 function MyTags(props) {
   const state = useSelector((s) => s);
@@ -8,14 +12,20 @@ function MyTags(props) {
   const { tags } = state;
 
   return (
-    <div>
-      <div>MyTags</div>
-      {tags.map((x) => (
-        <button key={x.name}>
-          {x.name} {x.count}
-        </button>
-      ))}
-    </div>
+    <Container>
+      <br />
+      <ButtonGroup
+        variant="contained"
+        aria-label="My Tags filter products by tags"
+      >
+        {tags.map((x) => (
+          <Button key={x.name}>
+            {x.name} {x.count}
+          </Button>
+        ))}
+      </ButtonGroup>
+      <br />
+    </Container>
   );
 }
 export default MyTags;
